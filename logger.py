1385feb972d5ec2164c98fce7fed63b860bd3efa
Your Name <youremail@example.com>
2025-04-09 13:59:46 +0300

    created LogEvent object; parsed events

--- logger.py
+++ logger.py
@@ -1,3 +1,24 @@
+import csv
+from datetime import datetime
+
+
+class LogEvent:
+
+    def __init__(self, timestamp, name, event, id):
+        self.id = id
+        self.name = name
+        self.event = event.strip()
+        self.time = datetime.strptime(timestamp, "%H:%M:%S").time() # parsed timestamp
+
+def parse_log(file_path):
+
+    events = []
+    with open(file_path) as f:
+        reader = csv.reader(f)
+        for row in reader:
+            events.append(LogEvent(*row))   # each row becomes a LogEvent
+    return events
+
 def main():
 
     file_path = "logs.log"  
